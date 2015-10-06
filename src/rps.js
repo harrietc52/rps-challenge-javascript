16712285698c864628431375376056f833e3b16e
Harriet Craven <harrietc52@hotmail.co.uk>
2015-10-06 11:55:46 +0100

    added computers random choice

--- src/rps.js
+++ src/rps.js
@@ -1,1 +1,13 @@
 var userChoice = prompt("Do you choose rock, paper or scissors?")
+
+var computerChoice = Math.random();
+
+console.log(computerChoice)
+
+if (computerChoice <= 0.33) {
+    computerChoice = "rock";
+} else if (computerChoice >= 0.34 && computerChoice <= 0.66) {
+       computerChoice = "paper";
+} else {
+        computerChoise = "scissors";
+};
